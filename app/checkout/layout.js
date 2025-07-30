c049167be199811ab7d187c55a4a1e68556ff8f3
Parth Dahiya <parth.dahiya01@gmail.com>
2025-07-31 03:02:44 +0530

    cahnged image extension case

--- app/checkout/layout.js
+++ app/checkout/layout.js
@@ -1,10 +1,11 @@
 export function generateMetadata() {
   return {
     title: "Checkout ",
-    description: "Complete your purchase at Premika Store. Secure checkout with multiple payment options including UPI, cards, and net banking. Fast delivery across India.",
+    description:
+      "Complete your purchase at Premika Store. Secure checkout with multiple payment options including UPI, cards, and net banking. Fast delivery across India.",
   };
 }
 
 export default function CheckoutLayout({ children }) {
-    return children;
+  return children;
 }
