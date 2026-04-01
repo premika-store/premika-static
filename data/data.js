8f3844abec75ebc4fa10377181702edf05908918
premika-store <premika.shop@gmail.com>
2026-04-01 19:45:48 +0530

    Set all clothing sizes to out of stock
    Updated stock availability for clothing sizes to false.

--- data/data.js
+++ data/data.js
@@ -384,12 +384,12 @@ const products = [
     category: "clothing",
     inStock: true,
     sizes: [
-      { label: "XXS", inStock: true },
-      { label: "XS", inStock: true },
-      { label: "S", inStock: true },
-      { label: "M", inStock: true },
-      { label: "L", inStock: true },
-      { label: "XL", inStock: true },
+      { label: "XXS", inStock: false},
+      { label: "XS", inStock: false },
+      { label: "S", inStock: false },
+      { label: "M", inStock: false },
+      { label: "L", inStock: false },
+      { label: "XL", inStock: false },
     ],
     reviews: [
       {
@@ -425,12 +425,12 @@ const products = [
     category: "clothing",
     inStock: true,
     sizes: [
-      { label: "XXS", inStock: true },
-      { label: "XS", inStock: true },
-      { label: "S", inStock: true },
-      { label: "M", inStock: true },
-      { label: "L", inStock: true },
-      { label: "XL", inStock: true },
+      { label: "XXS", inStock: false },
+      { label: "XS", inStock: false },
+      { label: "S", inStock: false },
+      { label: "M", inStock: false },
+      { label: "L", inStock: false },
+      { label: "XL", inStock: false },
     ],
     reviews: [
       {
